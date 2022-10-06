b6ac0c0b25ceb9eb63d343acc154bad13ddefe26
Irina Ten <virhot17@gmail.com>
2022-10-06 11:52:43 +0600

    Imrovement
    Модели зарегистрированы в админ-панели, добавлены данные. Созданы фикстуры с данными.

--- app/webapp/admin.py
+++ app/webapp/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Issue, Status, Type
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('id', 'summary', 'description', 'status', 'type')
+    list_filter = ('id', 'summary', 'status', 'type')
+    search_fields = ('summary', 'status', 'type')
+    fields = ('id', 'summary', 'description', 'status', 'type')
+    readonly_fields = ('id',)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_filter = ('id', 'title')
+    search_fields = ('title',)
+    fields = ('id', 'title')
+    readonly_fields = ('id',)
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_filter = ('id', 'title')
+    search_fields = ('title',)
+    fields = ('id', 'title')
+    readonly_fields = ('id',)
+
+
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Type, TypeAdmin)
--- app/webapp/models/statuses.py
+++ app/webapp/models/statuses.py
@@ -5,3 +5,7 @@ class Status(models.Model):
     title = models.CharField(verbose_name='Status', max_length=50, null=False, blank=False)
     created_at = models.DateTimeField(verbose_name='Date of created', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='Date of updates', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Status'
+        verbose_name_plural = 'Statuses'
