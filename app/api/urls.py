2e225097ec656aaec315f4d4ddac158f987f2d62
tenirina <virhot17@gmail.com>
2022-11-21 22:19:24 +0600

    Improvement
    Изменены URL адреса для читабельности кода

--- app/api/urls.py
+++ app/api/urls.py
@@ -6,10 +6,10 @@ from api.views import ProjectDetailView, ProjectListView, ProjectUpdateView, Pro
 urlpatterns = [
     path('projects/', ProjectListView.as_view(), name='projects_api'),
     path('project/<int:pk>', ProjectDetailView.as_view(), name='project_api'),
-    path('project/<int:pk>/update', ProjectUpdateView.as_view(), name='project_update_api'),
-    path('project/<int:pk>/delete', ProjectDeleteView.as_view(), name='project_delete_api'),
+    path('project/update/<int:pk>', ProjectUpdateView.as_view(), name='project_update_api'),
+    path('project/delete/<int:pk>', ProjectDeleteView.as_view(), name='project_delete_api'),
     path('issues/', IssueListView.as_view(), name='issues_api'),
     path('issue/<int:pk>', IssueDetailView.as_view(), name='issue_api'),
-    path('issue/<int:pk>/update', IssueUpdateView.as_view(), name='issue_update_api'),
-    path('issue/<int:pk>/delete', IssueDeleteView.as_view(), name='issue_delete_api'),
+    path('issue/update/<int:pk>', IssueUpdateView.as_view(), name='issue_update_api'),
+    path('issue/delete/<int:pk>', IssueDeleteView.as_view(), name='issue_delete_api'),
 ]
